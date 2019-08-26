4ca8f3f4e5bbc79859a249bedfec381039bf4445
axecopfire <sbraun990@gmail.com>
2019-08-25 20:44:00 -0400

    changed server to listen to https

--- index.js
+++ index.js
@@ -3,6 +3,6 @@ var http = require("http");
 http.createServer(function (req, res) {
 	res.end("Server connected")
 })
-	.listen(80, function() {
+	.listen(443, function() {
 		console.log("Connected");
 	});
