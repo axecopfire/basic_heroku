e7dbcfd11593962a20badefcad2d3849fe72ad07
axecopfire <sbraun990@gmail.com>
2019-08-25 20:49:21 -0400

    Updated ports

--- index.js
+++ index.js
@@ -3,6 +3,4 @@ var http = require("http");
 http.createServer(function (req, res) {
 	res.end("Server connected")
 })
-	.listen(443, function() {
-		console.log("Connected");
-	});
+	.listen(process.env.PORT || 3000);
